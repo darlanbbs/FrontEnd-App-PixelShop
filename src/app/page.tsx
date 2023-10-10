92d5606494f0f30f72e44617f0adeeb3004882ae
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-10 18:29:01 -0300

    Feat:Criação de context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import CatalogPage from "@/components/CatalogProducts/CatalogPage";
 import CatalogProductsArea from "@/components/CatalogProductsArea/CatalogProductsArea";
+import { MyContextProvider } from "@/components/context/ProductIdContext";
 
 export default function Home() {
   return (
     <main>
-      <CatalogProductsArea>
-        <CatalogPage />
-      </CatalogProductsArea>
+      <MyContextProvider>
+        <CatalogProductsArea>
+          <CatalogPage />
+        </CatalogProductsArea>
+      </MyContextProvider>
     </main>
   );
 }
